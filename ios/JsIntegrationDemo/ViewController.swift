014be9898e2129c405619f5767d20dfd7f3cd741
chocovayashi <c.tsujiyan727@gmail.com>
2020-09-03 11:27:26 +0900

    create xcode project

---
+++ ios/JsIntegrationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsIntegrationDemo
+//
+//  Created by 辻林　大揮 on 2020/09/03.
+//  Copyright © 2020 VOYAGE GROUP, INC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
